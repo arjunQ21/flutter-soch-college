417d5b9dcde53acb3c2eb10cd3b473f436c0e4b3
arjunQ21 <arjunq21@gmail.com>
2024-04-10 12:03:16 +0545

    Stack Demo

--- day5/lib/main.dart
+++ day5/lib/main.dart
@@ -1,180 +1,68 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: WeatherWidget(),
-    ),
-  );
+  runApp(MaterialApp(
+    home: StackDetails(),
+  ));
 }
 
-class WeatherWidget extends StatelessWidget {
-  WeatherWidget({
+class StackDetails extends StatelessWidget {
+  const StackDetails({
     super.key,
   });
-
-  TextStyle textKoStyle = TextStyle(
-    fontSize: 45,
-    height: 1,
-    fontWeight: FontWeight.bold,
-  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Weather Widget")),
+      appBar: AppBar(
+        title: Text("Stack Study"),
+      ),
       body: Center(
         child: Container(
-          color: Colors.red,
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.width * 0.8,
-          child: Row(
+          height: 300,
+          width: 300,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 2,
+            ),
+          ),
+          child: Stack(
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  StyledBox(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text(
-                            "12 C",
-                            style: TextStyle(
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text("Cloudy Day".toUpperCase())
-                        ]),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 160,
+                  width: 160,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    borderRadius: BorderRadius.circular(300),
                   ),
-                  StyledBox(
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Alarm"),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                "11:45",
-                                style: TextStyle(
-                                  fontSize: 25,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  bottom: 5,
-                                  left: 2,
-                                ),
-                                child: Text("AM"),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Text("S"),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("M"),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("T"),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text(
-                                "W",
-                                style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("T"),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text(
-                                "F",
-                                style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text("S"),
-                              SizedBox(
-                                width: 5,
-                              ),
-                            ],
-                          )
-                        ]),
-                  ),
-                ],
+                ),
               ),
-              StyledBox(
-                child: Column(children: [
-                  Text(
-                    "22",
-                    style: textKoStyle,
-                  ),
-                  Transform.rotate(
-                    angle: 3.14 / 2,
-                    child: Text(
-                      ":",
-                      style: textKoStyle,
+              Positioned(
+                top: 120,
+                left: 60,
+                child: Container(
+                  height: 100,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
                     ),
                   ),
-                  Text(
-                    "56",
-                    style: textKoStyle,
-                  ),
-                  Text("MON 2 MAY, 2022"),
-                  Text("RAY"),
-                ]),
-                height: 300,
-              )
+                ),
+              ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class StyledBox extends StatelessWidget {
-  final Widget child;
-  double height;
-  StyledBox({
-    required this.child,
-    this.height = 140,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: 140,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 4,
-            offset: Offset(4, 4),
-            spreadRadius: 3,
-            color: Colors.black.withOpacity(0.4),
-          )
-        ],
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: child,
     );
   }
 }
