b99503deffea17115c271c4ec8849aa64c990bb3
arjunQ21 <arjunq21@gmail.com>
2024-04-18 11:58:27 +0545

    Day 7: Parameters Sending

--- day7/lib/main.dart
+++ day7/lib/main.dart
@@ -19,40 +19,80 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text("Multiplication Table"),
       ),
-      body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text("Hello From Home Page"),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                // CupertinoPageRoute(
-                MaterialPageRoute(
-                  builder: (c) => SecondPage(),
-                ),
-              );
-            },
-            child: Text("Click Me"),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "Click on a Number to see its table.",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            Wrap(
+              children: [
+                for (int i = 1; i <= 10; i++)
+                  NumberTile(
+                    numberToShow: i,
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NumberTile extends StatelessWidget {
+  int numberToShow;
+  NumberTile({
+    super.key,
+    required this.numberToShow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            // CupertinoPageRoute(
+            MaterialPageRoute(
+              builder: (c) => SecondPage(
+                tableNumber: numberToShow,
+              ),
+            ),
+          );
+        },
+        child: Text(
+          numberToShow.toString(),
+          style: TextStyle(
+            fontSize: 20,
           ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
 
 class SecondPage extends StatelessWidget {
-  const SecondPage({
+  int tableNumber;
+  SecondPage({
     super.key,
+    required this.tableNumber,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Second Page"),
+        title: Text(this.tableNumber.toString()),
       ),
       body: Center(
           child: Column(
@@ -61,9 +101,10 @@ class SecondPage extends StatelessWidget {
           Text("Hello From Second Page"),
           ElevatedButton(
             onPressed: () {
-              print("Button Clicked");
+              Navigator.of(context).pop();
+              // Navigator.pop(context);
             },
-            child: Text("Click Me"),
+            child: Text("Go Back"),
           ),
         ],
       )),
