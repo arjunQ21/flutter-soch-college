cc25906c4a24eb56d888bcd784915ffc3bcd4091
arjunQ21 <arjunq21@gmail.com>
2024-04-29 12:11:58 +0545

    Day 15: Futures: Success and Error Handling

--- day15/async.dart
+++ day15/async.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 int count = 0;
 void sayHi() {
   print("${++count}: Hello World");
@@ -9,11 +11,38 @@ Future<void> waitFor(int seconds) async {
   });
 }
 
+Future<void> showNextSecondAfter(int afterWhat) async {
+  if (afterWhat > 5) return;
+  await waitFor(1);
+  print("$afterWhat seconds.");
+  await showNextSecondAfter(afterWhat + 1);
+}
+
+// a function that return a random number after 2 seconds.
+Future<int> randomNo() {
+  return Future.delayed(Duration(seconds: 1), () {
+    int genNo = Random().nextInt(5);
+    if (genNo > 2) throw Exception("2 vanda thulo vayo");
+    return genNo;
+  });
+}
+
 void main() async {
-  // sayHi();
-  for (int i = 0; i < 10; i++) {
-    print("${i + 1} seconds");
-    await waitFor(1);
+// Error handling
+ // Callback Style
+  await randomNo().then((value) {
+    print("Random number: $value");
+  }).catchError((error) {
+    print("Error caught from callback style. Error says: " + error.toString());
+  });
+
+// Synchronous style
+  try {
+    int randomInt = await randomNo();
+    print("Random number: $randomInt");
+  } catch (e) {
+    print("Error caught synchronously. Error says: " + e.toString());
   }
-  // sayHi();
+
+ 
 }
