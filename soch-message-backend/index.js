f73cb2d58639acfd50acd75cb4fc5e866b51e357
arjunQ21 <arjunq21@gmail.com>
2024-05-09 11:17:10 +0545

    Adding and removing messages in JSON format.

--- soch-message-backend/index.js
+++ soch-message-backend/index.js
@@ -11,10 +11,17 @@ app.get('/', (req, res) => {
 app.post("/messages", function(req, res){
     try{
 if(req.body.message){
-fs.appendFileSync("message.txt" ,req.body.message.trim() + "\n", {encoding: 'utf-8'}) ;
-var contents = fs.readFileSync("message.txt", {encoding: "utf-8"}) ;
+    var contents = fs.readFileSync("messages.json", {encoding: "utf-8"}) ;
+    var messages = JSON.parse(contents) ;
+// fs.appendFileSync("message.txt" ,req.body.message.trim() + "\n", {encoding: 'utf-8'}) ;
+var newMessage = {
+    id: messages.length > 0 ? messages[messages.length - 1]['id'] + 1 : 1,
+    message: req.body.message
+} ;
+messages.push(newMessage)
+fs.writeFileSync("messages.json", JSON.stringify(messages), {encoding: 'utf-8'} ) ;
 res.send({status: "success",data: {
-    messages: contents ? contents.split("\n").reverse().filter(e => !!e) : []
+    message: newMessage
 }})
 }else{
     throw new Error("No message found in request") ;
@@ -30,11 +37,79 @@ res.send({
     }
 })
 
+
+function getSavedMessages(){
+    return JSON.parse(fs.readFileSync("messages.json", {encoding: "utf-8"})) ;
+}
+
+const singleMessageRouter = express.Router() ;
+
+app.use("/messages/:id", function(req, res, next){
+    const messages = getSavedMessages() ;
+    const hasId = messages.some(function (e, i, a){
+        return e['id'].toString() == req.params.id.toString()
+    }) ;
+    if(!hasId){
+        return res.send({
+            status: "fail",
+            data: {
+                error: "No id found: "+ req.params.id
+            }
+        })
+    }else {
+        req.messageId = req.params.id ;
+        return next()} ;
+}, singleMessageRouter)
+
+singleMessageRouter.put("/", function(req, res){
+try{
+if(!req.body.message) throw new Error("Message not found in request") ;
+let messages = getSavedMessages() ;
+for(let i = 0 ; i < messages.length; i++){
+    if(messages[i]['id'] == req.messageId){
+        messages[i]['message'] = req.body.message.trim() ;
+        fs.writeFileSync("messages.json", JSON.stringify(messages), {encoding: 'utf-8'}) ;
+        return res.send({
+            status: "success",
+            data: {
+                message: messages[i]
+            }
+        })
+    }
+}
+return res.send({
+    status: "error",
+    data: {
+        message: "Not found."
+    }
+})
+}catch(e){
+    return res.send({
+        status: "error",
+        data: {
+            message: e.toString()
+        }
+    })
+}
+})
+
+
+singleMessageRouter.delete("/", function(req, res){
+    const filtered = getSavedMessages().filter(e => e['id'].toString() != req.messageId.toString() )
+    console.log(filtered)
+    fs.writeFileSync( "messages.json", JSON.stringify(filtered) , {encoding: 'utf-8'}) ;
+    return res.send({
+        status: "success",
+        data: {}
+    })
+})
+
+
 app.get("/messages", function(req, res){
-    var contents = fs.readFileSync("message.txt", {encoding: "utf-8"}) ;
-    res.send({status: "success",data: {
-        messages: contents ? contents.split("\n").reverse().filter(e => !!e) : []
-    }})
+    return res.send({
+        status: "success",
+        data: getSavedMessages()
+    })
 })
 
 app.listen(port, () => {
