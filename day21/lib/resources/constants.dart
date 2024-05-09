0a857e530608f35f737e6a87a816fcdae3434959
arjunQ21 <arjunq21@gmail.com>
2024-05-09 12:50:35 +0545

    Add, Read, Edit, Delete messages done from API Integration

---
+++ day21/lib/resources/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color pColor = Color.fromARGB(255, 197, 58, 16);
+Color sColor = Colors.white;
+Color textFieldBg = Colors.grey;
+Color iconColor = Colors.grey;
+
+const String apiURL = "http://10.0.2.2:3000";
+
+String randomImgUrl = 'https://picsum.photos/200/300?random=';
